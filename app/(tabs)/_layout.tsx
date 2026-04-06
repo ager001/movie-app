5bd8faff5100d61d010b20aa7cb701f4419a0330
ager001 <152964009+ager001@users.noreply.github.com>
2026-04-06 22:52:02 +0300

    feat: restructure app layout and add new screens for tabs navigation

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import { Tabs } from 'expo-router';
+
+const _layout = () => {
+  return (
+    <Tabs>
+      <Tabs.Screen name="index" options={{ 
+        title: "Home",
+        headerShown: false }} />
+    </Tabs>
+  );
+};
+
+export default _layout;
---
+++ app/movies/[id].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const movieDetails = () => {
+  return (
+    <View className="flex-1">
+      <Text>movieDetails</Text>
+    </View>
+  );
+};
+
+export default movieDetails;
